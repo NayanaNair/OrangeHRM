e38fddce8a1ccd1dff1a41b9d679cfb957ba2bcc
Your Name <nainanair29@gmail.com>
2023-02-26 21:48:07 +0530

    first commit

---
+++ AddEmployee.java
@@ -0,0 +1,50 @@
+
+
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+
+public class AddEmployeePage extends TestBase{
+	
+	//Page Factory or Object Repository
+	
+	
+
+			@FindBy(xpath="//input[@name='firstName']")
+			WebElement firstName;
+			
+			@FindBy(xpath="//input[@name='middleName']")
+			WebElement middleName;
+			
+			@FindBy(xpath="//input[@name='lastName']")
+			WebElement lastName;
+			
+			@FindBy(xpath="//button[text()=' Save ']")
+			WebElement submitBtn;
+			
+			@FindBy(xpath="//button[text()=' Cancel ']")
+			WebElement cancelBtn;
+
+			//Initializing Page objects:
+
+			public AddEmployeePage() {
+				PageFactory.initElements(driver, this);
+			}
+
+			//Actions:
+			
+			public PersonalDetailsPage addNewEmployee(String ftName, String mdName, String ltName ) {
+				
+				firstName.sendKeys(ftName);
+				middleName.sendKeys(mdName);
+				lastName.sendKeys(ltName);
+				
+				submitBtn.click();
+				
+				return new PersonalDetailsPage();
+			}
+
+}
---
+++ ResetPasswordPage.java
@@ -0,0 +1,43 @@
+
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+
+public class ResetPasswordPage extends TestBase{
+
+	//Page Factory or Object Repository
+
+	@FindBy(xpath="//input[@name='username']")
+	WebElement username;
+
+	@FindBy(xpath="//button[text()=' Cancel ']")
+	WebElement cancelBtn;
+
+	@FindBy(xpath="//button[text()=' Reset Password ']")
+	WebElement resetPasswordBtn;
+
+	//Initializing Page objects:
+
+	public ResetPasswordPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Actions:
+	
+	public ResetPasswordPage resetPassword(String un) {
+		username.sendKeys(un);
+		resetPasswordBtn.click();
+		return new ResetPasswordPage();
+	}
+	
+	public LoginPage resetPasswordCancel(String un) {
+		username.sendKeys(un);
+		cancelBtn.click();
+		return new LoginPage();
+	}
+
+
+}
